38d78622158d8a1cd68cc67ac49d7d70f16ab3c6
Bruno Pires <brunopires.ctto@gmail.com>
2023-08-23 12:58:48 -0300

    Consulta simples com query methods

--- src/main/java/com/brunopires/workshopmongo/resources/PostResources.java
+++ src/main/java/com/brunopires/workshopmongo/resources/PostResources.java
@@ -1,13 +1,17 @@
 package com.brunopires.workshopmongo.resources;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.brunopires.workshopmongo.domain.Post;
+import com.brunopires.workshopmongo.resources.util.URL;
 import com.brunopires.workshopmongo.services.PostService;
 //--> OS ENDPOINTS SÃO RESPONSAVEIS PELA REQUISIÇÃO NO HTTP, POR EX: /USERS /ID /PUT /GET ETC... <--//
 @RestController
@@ -24,5 +28,11 @@ public class PostResources {
 		return ResponseEntity.ok().body(obj);
 	}
 	
+	@GetMapping(value = "/titlesearch")                 // METODO DE LOCALIZAR POR SEARCH"TEXT"
+	public ResponseEntity<List<Post>> findByTitle(@RequestParam(value = "text", defaultValue = "") String text){
+		text = URL.decodeParam(text);
+		List<Post> list = service.findByTitle(text);
+		return ResponseEntity.ok().body(list);
+	}
 	
 }
